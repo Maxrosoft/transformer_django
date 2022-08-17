9ba425e30cc55a1af148fd5a2a1a622dd3377141
Maxrosoft <100025129+Maxrosoft@users.noreply.github.com>
2022-08-17 08:50:47 +0200

    two new template tags created

--- main/templatetags/main_tags.py
+++ main/templatetags/main_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from ..models import Post
+from django.db.models import Count
 
 register = template.Library()
 
@@ -7,3 +8,16 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+
+@register.inclusion_tag('main/posts/post/latest_posts.html')
+def get_latest_posts(count=5):
+    latest_posts = Post.published.all()[:count]
+    return {'latest_posts': latest_posts}
+
+
+@register.inclusion_tag('main/posts/post/most_commented_posts.html')
+def get_most_commented_posts(count=5):
+    most_commented_posts = Post.published.annotate(
+        total_comments=Count('comments')).order_by('-total_comments')[:count]
+    return {'most_commented_posts': most_commented_posts}
